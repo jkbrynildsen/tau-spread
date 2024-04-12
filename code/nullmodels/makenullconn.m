20002b02576c9f974fac15f392154907075fbb9d
jkbrynildsen <66489067+jkbrynildsen@users.noreply.github.com>
2024-04-12 12:52:37 -0400

    Delete code directory
    Deleted code from previous version before replacing with updated code

--- code/nullmodels/makenullconn.m
+++
@@ -1,8 +0,0 @@
-cd(homedir); addpath(genpath(pwd));
-load(fullfile(savedir,'W.mat'));
-
-%% degree sequence preserving rewiring
-Wrw_in = dir_generate_srand(W',1e8)';
-Wrw_out = dir_generate_srand(W,1e8);
-
-save(fullfile(savedir,'Wrewire.mat'),'Wrw_in','Wrw_out');
